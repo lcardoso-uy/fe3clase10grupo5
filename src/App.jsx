8f7a26c420424c9ef970391f14c754a2114f5107
lcardoso-uy <lcardosogx@gmail.com>
2023-08-24 22:42:48 -0300

    agregamos botón

--- src/App.jsx
+++ src/App.jsx
@@ -20,17 +20,31 @@ function App() {
 
   }, [])
 
+  function handlerConsejo(){
+    fetch("https://api.adviceslip.com/advice")
+      .then((response) => response.json())
+      .then((data) => {
+        setConsejo(data.slip.advice);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("error al obtener una respuesta - ", error);
+      });
+  }
+
   return (
     <div className='App'>
       <h1>Tu consejo personalizado</h1>
+      <p>   </p>      
       {loading ? (
         <p>Cargando...</p>
       ) : (
         <div>
           <h1>{consejo}</h1>
+          <p>   </p>
+          <button onClick={handlerConsejo}>Nuevo consejo</button>
         </div>
       )}
-
     </div>
   )
 }
